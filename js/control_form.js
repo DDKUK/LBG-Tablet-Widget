4839cfa107d77cb79577e07270f5df2e60fa6e2e
DDKUK <daniel@pixelinspiration.co.uk>
2015-05-01 10:20:43 +0100

    Moved Form Factor, position and brand info to a csv
    setup form factor to be dependant on position

--- js/control_form.js
+++ js/control_form.js
@@ -40,3 +40,75 @@
 	
         }
 	});
+
+	$.ajax({
+	    url: "http://localhost:8000//LBG_tablet_config.csv",
+	    async: false,
+	    success: function (csvd) {
+	        data2 = $.csv2Array(csvd);
+	    },
+ 	    dataType: "text",
+	    complete: function () {
+ 	    // call a function on complete 
+	    
+	    headers = data2[0];
+
+	    data2 = data2.splice(1, data.length - 1);
+
+	    for (var row in data2)
+	    {		
+			if (data2[row][headers.indexOf('brand')] != '')
+			{
+				var option = document.createElement("option");
+				option.value = data2[row][headers.indexOf('brand')];
+				option.text  = data2[row][headers.indexOf('brand')];
+				var select = document.getElementById("selectbox_brand");
+				select.appendChild(option);
+			}	
+	    }
+
+	    for (var pos in headers)
+	    {
+			if (pos >0)
+			{
+				var option = document.createElement("option");
+				option.value = headers[pos];
+				option.text  = headers[pos];
+				var select = document.getElementById("selectbox_position");
+				select.appendChild(option);
+			}
+	    }
+
+        }
+	});
+
+	function update_FormFactor() {
+
+		removeOptions(document.getElementById("selectbox_formfactor"));    			
+
+	    	for (var row in data2)
+	    	{		
+			if (data2[row][headers.indexOf(document.getElementById("selectbox_position").value)] != '')
+			{
+				var option = document.createElement("option");
+				option.value = data2[row][headers.indexOf(document.getElementById("selectbox_position").value)];
+				option.text  = data2[row][headers.indexOf(document.getElementById("selectbox_position").value)];
+				var select = document.getElementById("selectbox_formfactor");
+				select.appendChild(option);
+			}	
+	    	}
+	}
+
+	function removeOptions(selectbox)
+	{
+	    var i;
+	    for(i=selectbox.options.length-1;i>=0;i--)
+	    {
+	        selectbox.remove(i);
+	    }
+
+	    var option = document.createElement("option");
+	    option.value = "0";
+	    option.text  = "~Please Select~";
+	    selectbox.appendChild(option);
+	}
